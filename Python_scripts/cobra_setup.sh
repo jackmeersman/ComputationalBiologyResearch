40c374ce1be40afa87d5ed4e83d7428b25897cd0
Jack Meersman <jmeers1996@users.noreply.github.com>
2017-04-25 13:38:37 -0400

    Create cobra_setup.sh

---
+++ Python_scripts/cobra_setup.sh
@@ -0,0 +1,11 @@
+pip install pip --upgrade
+pip install pytest
+pip install pytest-benchmarch
+pip install python-libsbml
+pip install lxml
+pip install scipy
+pip install glpk
+pip install cobra
+pip install xlwt
+pip install xlsxwriter
+pip install pandas
